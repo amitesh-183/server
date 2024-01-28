af3e1c7225ddbc83fca25d130c4ac1bf2435bd58
Jameskillr <amiteshjames00718@gmail.com>
2024-01-28 20:18:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+require("dotenv").config({ path: "./config/config.env" });
+
+const port = process.env.PORT || 4000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from server");
+});
+
+app.listen(port, () =>
+  console.log(`Server is running at http://localhost:${port}`)
+);
