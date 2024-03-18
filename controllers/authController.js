0bc2f87adf04b9519dc982d46aaca553d89c3320
Rishabh Singh <rishabh.singh@rungta.org>
2024-03-18 22:31:33 +0530

    mybackend

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,35 +1,44 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
-const register = (req, res) => {
-  res.send({ msg: "successfully registered" });
+const register = async (req, res) => {
   const { email, username, password } = req.body;
-  User.create({ email, username, password });
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10); // Hash the password
+    const user = new User({ email, username, password: hashedPassword });
+    await user.save(); // Save the user to the database
+    res.send({ msg: "successfully registered" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Internal server error" });
+  }
 };
+
 const login = async (req, res) => {
-  res.send({ msg: "successfully login" });
   const { username, password } = req.body;
   if (!username || !password)
     return res.status(400).json({ msg: "Missing fields" });
 
-  // Simple validation
-  if (username.length < 5 || password.length < 6) {
-    return res.status(400).json({
-      msg: "Username and password must be at least 5 and 6 characters long, respectively.",
-    });
-  } else if (
-    User.findOne(password) !== password ||
-    User.findOne(username) !== username
-  ) {
-    return res.status(400).json({ msg: "Incorrect username or password" });
+  try {
+    const user = await User.findOne({ username }); // Find the user by username
+    if (!user) {
+      return res.status(400).json({ msg: "User not found" });
+    }
+
+    // Compare provided password with hashed password stored in the database
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(400).json({ msg: "Incorrect password" });
+    }
+
+    res.status(200).json({ msg: "Successfully logged in" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Internal server error" });
   }
-  return res.status(400).json({ msg: "Successfully logged In" });
-};
-const logout = (req, res) => {
-  res.send({ msg: "successfully logout" });
 };
 
 module.exports = {
   register,
   login,
-  logout,
 };
